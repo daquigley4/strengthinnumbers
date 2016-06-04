82ced70ddac7ddb31a5af571d97aefb8172068fa
David Quigley <daquigley4@gmail.com>
2016-06-04 16:23:08 -0400

    Added gitignore file and edited index view.

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -2,6 +2,16 @@ angular.module('strengthInNumbers', []);
 
 angular.module('strengthInNumbers')
 .controller('playersCtrl', function($http) {
-  var ctrl = this;
   console.log('playersCtrl is alive!');
+
+  var ctrl = this;
+
+  ctrl.getPlayers = function() {
+    $http.get('/api/players').then(function(response) {
+      ctrl.players = response.data;
+      console.log('ctrl.players: ', ctrl.players)
+    });
+  };
+
+  ctrl.getPlayers();
 });
