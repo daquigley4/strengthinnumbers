1bd7f0a316ebcd86e14ecd514ae3ae68c66fa98b
David Quigley <daquigley4@gmail.com>
2016-06-04 15:04:16 -0400

    Installed bower, created initial route and player model.

---
+++ public/javascripts/client.js
@@ -0,0 +1,7 @@
+angular.module('strengthInNumbers', []);
+
+angular.module('strengthInNumbers')
+.controller('playersCtrl', function($http) {
+  var ctrl = this;
+  console.log('playersCtrl is alive!');
+});
--- routes/players.js
+++ routes/players.js
@@ -1,1 +1,25 @@
 var express = require('express');
+
+var Player = require('../models/player');
+
+function seedPlayers() {
+  var players = [
+    { name: 'LeBron James', team: 'Cleveland Cavaliers',        age: 31 },
+    { name: 'Kevin Durant', team: 'Oklahoma City Thunder',      age: 28 },
+    { name: 'Steph Curry', team: 'Golden State Warriors',       age: 28 },
+    { name: 'Russell Westbrook', team: 'Oklahoma City Thunder', age: 27 }
+  ];
+
+  Player.find({}).remove()
+  .then(function() {
+    return Player.create(players);
+  })
+  .then(function() {
+    return Player.find({});
+  })
+  .then(function(found) {
+    console.log('We saved and retrieved', found.length, 'players.');
+  });
+}
+
+seedPlayers();
