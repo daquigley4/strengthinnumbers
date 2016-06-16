12839d3b109196bfbb8b293ed02fdbab739c93ac
David Quigley <daquigley4@gmail.com>
2016-06-16 11:10:18 -0400

    Cleaned up code and added copyright.

--- models/favorite.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-
-var FavoriteSchema = new mongoose.Schema({
-  name:  {type: String, required: true },
-  playerName: {type: String, required: true}
-}, {timestamps: true});
-
-module.exports = mongoose.model('Favorite', FavoriteSchema);
